85ac9ca919d520f6ea12513c9b6f559c53e9cf7a
JevinMakwana <makwanajevin413@gmail.com>
2024-04-29 01:06:24 +0530

    Cors and Throttler setting implemented - 01

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeeModule],
+  imports: [
+    UsersModule, 
+    DatabaseModule, 
+    EmployeeModule,
+    ThrottlerModule.forRoot([{
+      ttl:60000, //ttl: time to live //
+      limit: 3, // 3 req per 60000 ms
+    }])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule {}
